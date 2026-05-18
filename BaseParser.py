cd9a8a316ed0fa5f3bf1bb0faad4fe793450221b
Aspirinder <atalmachou1302@gmail.com>
2026-05-18 18:46:53 +0200

    feat: implement multi-page scraping for OLX and Otodom

---
+++ BaseParser.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class BaseParser(ABC):
+    @abstractmethod
+    async def parse_list(self, content):
+        # Method of collecting links from the main page
+        pass
+
+    @abstractmethod
+    async def parse_details(self, page, ad_item):
+        # Method for collecting descriptions and photos inside an ad
+        pass
+
+    @abstractmethod
+    def change_page(self, url, number):
+        # Method for change page on website
+        pass
--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import asyncio
-from bs4 import BeautifulSoup
-import pandas as pd
 from playwright.async_api import async_playwright
-from OLXParser import OLXParser
+from ScraperEngine import ScraperEngine
+from websites.OLXParser import OLXParser
+from websites.OTODOMParser import OTODOMParser
 
-BASE_URL = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/poznan/?page=1&search%5Bfilter_enum_furniture%5D%5B0%5D=yes&search%5Bfilter_enum_rooms%5D%5B0%5D=two&search%5Bfilter_float_price%3Ato%5D=3000"
-OUTPUT_FILE = "olx_offers_data.csv"
+OLX_URL = "https://www.olx.pl/nieruchomosci/mieszkania/wynajem/poznan/?page=1&search%5Bfilter_enum_furniture%5D%5B0%5D=yes&search%5Bfilter_enum_rooms%5D%5B0%5D=two&search%5Bfilter_float_price%3Ato%5D=3000"
+OTODOM_URL = "https://www.otodom.pl/pl/wyniki/wynajem/mieszkanie,2-pokoje/wielkopolskie/poznan/poznan/poznan?limit=36&priceMax=3000&by=DEFAULT&direction=DESC&page=1"
+OLX_OUTPUT_FILE = "CSV/olx_offers_data.csv"
+OTODOM_OUTPUT_FILE = "CSV/otodom_offers_data.csv"
 LIMIT = 5
 
 async def main():
     
-    parser = OLXParser(output_file=OUTPUT_FILE)
+    engine = ScraperEngine(output_file=OLX_OUTPUT_FILE)
+    await engine.start_browser(headless=False)
+
+    page_number = 16
+
+    olx = OLXParser()
+    otodom = OTODOMParser()
     
-    try:
-        await parser.start_browser(headless=False)
-
-        for i in range(1, 17):
-            # Get info about offers on search page
-            corrent_url = BASE_URL.replace("page=1", f"page={i}")
-
-            offers = await parser.get_main_page_data(corrent_url)
+    while(True):
         
-            # Open every offer and get data
-            for offer in offers:
-                offer_datails = await parser.fetch_details(offer)
-                parser.results.append(offer_datails)
+        found_count = await engine.run_parser(olx, olx.change_page(OLX_URL, page_number))
 
+        if found_count == 0:
+            print(f"✅ All pages processed. Finished at page {page_number - 1}.")
+            break
+
+        page_number += 1
+    engine.save_to_csv()
+
+    engine.output_file = OTODOM_OUTPUT_FILE
+    engine.results = []
+    page_number = 15
+
+    while(True):
+
+        found_count, content = await engine.run_parser(otodom, otodom.change_page(OTODOM_URL, page_number))
+
+        can_go_next = await otodom.has_next_page(content)
+
+        if found_count == 0 or not can_go_next:
+            print(f"✅ All pages processed. Finished at page {page_number}.")
+            break
         
-        parser.save_to_csv()
-        
-    finally:
-        await parser.stop_browser()
+        page_number += 1
+
+    engine.save_to_csv()
+    await engine.stop_browser()
 
 if __name__ == "__main__":
     asyncio.run(main())
